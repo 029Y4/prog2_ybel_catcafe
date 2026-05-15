eb497417395b6be97d929c6195399e1ba79c2796
029Y4 <martin.fenske@hsbi.de>
2026-05-16 00:10:20 +0200

    add tests for getCatCount and getCatByName methods

--- src/test/java/catcafe/CatCafeTest.java
+++ src/test/java/catcafe/CatCafeTest.java
@@ -3,6 +3,8 @@ package catcafe;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class CatCafeTest {
 
@@ -29,6 +31,7 @@ class CatCafeTest {
     // when & then
     assertThrows(NullPointerException.class, () -> catcafe.addCat(null));
   }
+/*
 
   @Test
   void addCat_throws_nullPointerException_when_empty_string_is_added() {
@@ -38,4 +41,85 @@ class CatCafeTest {
       // when & then
       assertThrows(NullPointerException.class, () -> catcafe.addCat(""));
   }
+*/
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 5, 100})
+    void getCatCount_returns_correct_number_of_cats(int catCount) {
+        // given
+        CatCafe catcafe = new CatCafe();
+        for (int i = 0; i < catCount; i++) catcafe.addCat(new FelineOverLord("Cat" + i, 1));
+        // Schlägt fehl wenn weight für alle = 1 ist
+        // getCatCount gibt dann immer nur eine Katze zurück
+
+        // when
+        long res = catcafe.getCatCount();
+
+        // then
+        assertEquals(catCount, res);
+    }
+
+    @Test
+    void getCatByName_returns_correct_cat_when_one_cat_exists() {
+        // given
+        String testName = "Erna";
+        FelineOverLord testCat = new FelineOverLord(testName, 4);
+
+        CatCafe catcafe = new CatCafe();
+        catcafe.addCat(testCat);
+
+        // when
+
+        FelineOverLord res = catcafe.getCatByName(testName);
+
+        // then
+        assertEquals(testCat, res);
+    }
+
+    @Test
+    void getCatByName_returns_correct_cat_when_multiple_cats_exist() {
+        // given
+        CatCafe catcafe = new CatCafe();
+
+        FelineOverLord targetCat = new FelineOverLord("Erna", 4);
+        FelineOverLord paul = new FelineOverLord("Magister Paul", 5);
+
+        catcafe.addCat(targetCat);
+        catcafe.addCat(paul);
+
+        // when
+        FelineOverLord res = catcafe.getCatByName("Erna");
+
+        // then
+        assertEquals(res, targetCat);
+    }
+
+    @Test
+    void getCatByName_returns_null_when_catcafe_is_empty() {
+        // given
+        String testName = "Magister Paul";
+
+        CatCafe catcafe = new CatCafe();
+
+        // when
+        FelineOverLord res = catcafe.getCatByName(testName);
+
+        // then
+        assertNull(res);
+    }
+
+    @Test
+    void getCatByName_returns_null_when_searching_by_null_name() {
+        // given
+        CatCafe catcafe = new CatCafe();
+        catcafe.addCat(new FelineOverLord("Magister Paul", 5));
+        catcafe.addCat(new FelineOverLord("Erna", 4));
+
+        // when
+        FelineOverLord res = catcafe.getCatByName(null);
+
+        // then
+        assertNull(res);
+    }
+
 }
