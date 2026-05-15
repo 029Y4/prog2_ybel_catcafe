a0b5c438b655b4480be4c97e26a08d4aed88e416
029Y4 <martin.fenske@hsbi.de>
2026-05-16 00:05:24 +0200

    add tests for addCat method

--- src/test/java/catcafe/CatCafeTest.java
+++ src/test/java/catcafe/CatCafeTest.java
@@ -2,6 +2,40 @@ package catcafe;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Test;
+
 class CatCafeTest {
 
+  @Test
+  void addCat_adds_cats_successfully() {
+    // given
+    CatCafe catcafe = new CatCafe();
+    FelineOverLord erna = new FelineOverLord("Erna", 4);
+
+    catcafe.addCat(new FelineOverLord("Magister Paul", 5));
+    catcafe.addCat(erna);
+    // when
+    FelineOverLord res = catcafe.getCatByName("Erna");
+
+    // then
+    assertEquals(erna, res);
+  }
+
+  @Test
+  void addCat_throws_nullPointerException_when_adding_null_cat() {
+    // given
+    CatCafe catcafe = new CatCafe();
+
+    // when & then
+    assertThrows(NullPointerException.class, () -> catcafe.addCat(null));
+  }
+
+  @Test
+  void addCat_throws_nullPointerException_when_empty_string_is_added() {
+      // given
+      CatCafe catcafe = new CatCafe();
+
+      // when & then
+      assertThrows(NullPointerException.class, () -> catcafe.addCat(""));
+  }
 }
